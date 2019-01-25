c2026f202073a15f621f3c624006edf457d9ad6f
Alex Marchant <alexjmarchant@gmail.com>
2019-01-24 23:17:44 -0500

    Add current user stuff to picks controller

--- picks_controller.go
+++ picks_controller.go
@@ -4,28 +4,33 @@ import (
   "github.com/gorilla/mux"
   "encoding/json"
   "net/http"
-  "strconv"
   "log"
   "fmt"
 )
 
 func PicksRegisterHandlers(r *mux.Router) {
-  r.HandleFunc("/users/{id}/picks", PicksReadHandler).Methods("GET")
-  r.HandleFunc("/users/{id}/picks", PicksUpdateHandler).Methods("POST")
+  r.HandleFunc("/users/current-user/picks", PicksReadHandler).Methods("GET")
+  r.HandleFunc("/users/current-user/picks", PicksUpdateHandler).Methods("POST")
 }
 
 func PicksReadHandler(w http.ResponseWriter, r *http.Request) {
-  vars := mux.Vars(r)
-  id := vars["id"]
+  // Parse token info
+  claims, err := getAuthTokenClaims(r)
+  if err != nil {
+    w.WriteHeader(http.StatusBadRequest)
+    SendJson(w, JsonError{ Error: "Invalid token" })
+    log.Printf("Invalid token: %v", err)
+    return
+  }
 
   // Get picks
   var picks string
-  err := db.QueryRow("SELECT picks FROM users WHERE id = $1", id).Scan(&picks)
+  err = db.QueryRow("SELECT picks FROM users WHERE id = $1", claims.Id).Scan(&picks)
   if err != nil {
     if err.Error() == "sql: no rows in result set" {
       w.WriteHeader(http.StatusNotFound)
-      SendJson(w, JsonError{ Error: fmt.Sprintf("No user found with id %s", id) })
-      log.Printf("No user found with id %s", id)
+      SendJson(w, JsonError{ Error: fmt.Sprintf("Can't find user") })
+      log.Printf("Can't find user: %v", err)
     } else {
       w.WriteHeader(http.StatusInternalServerError)
       SendJson(w, JsonError{ Error: "Error querying database" })
@@ -43,23 +48,12 @@ func PicksReadHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func PicksUpdateHandler(w http.ResponseWriter, r *http.Request) {
-  vars := mux.Vars(r)
-  id := vars["id"]
-
-  // Parse id to int
-  id64, err := strconv.ParseInt(id, 10, 64)
+  // Parse token info
+  claims, err := getAuthTokenClaims(r)
   if err != nil {
     w.WriteHeader(http.StatusBadRequest)
-    SendJson(w, JsonError{ Error: "Invalid id" })
-    log.Print("Invalid id")
-    return
-  }
-
-  // Authorize user
-  if !AuthorizeUser(r, id64) {
-    w.WriteHeader(http.StatusBadRequest)
-    SendJson(w, JsonError{ Error: "Not authorized to edit this user's picks" })
-    log.Print("Not authorized to edit this user's picks")
+    SendJson(w, JsonError{ Error: "Invalid token" })
+    log.Printf("Invalid token: %v", err)
     return
   }
 
@@ -74,20 +68,6 @@ func PicksUpdateHandler(w http.ResponseWriter, r *http.Request) {
     return
   }
 
-  // Validate data
-  valid := true
-  for _, pick := range body {
-    if pick.Category == "" || pick.Pick == "" {
-      valid = false
-    }
-  }
-  if !valid {
-    w.WriteHeader(http.StatusBadRequest)
-    SendJson(w, JsonError{ Error: "Invalid request body" })
-    log.Print("Invalid request body")
-    return
-  }
-
   // Turn request into string for postgres
   jsonBodyBytes, err := json.Marshal(body)
   if err != nil {
@@ -98,11 +78,11 @@ func PicksUpdateHandler(w http.ResponseWriter, r *http.Request) {
   }
 
   // Update postgres data
-  _, err = db.Exec("UPDATE users SET picks = $1 WHERE id = $2", string(jsonBodyBytes), id)
+  _, err = db.Exec("UPDATE users SET picks = $1 WHERE id = $2", string(jsonBodyBytes), claims.Id)
   if err != nil {
     w.WriteHeader(http.StatusInternalServerError)
-    SendJson(w, JsonError{ Error: "Error updating database" })
-    log.Printf("Error updating database: %v", err)
+    SendJson(w, JsonError{ Error: "Error updating picks" })
+    log.Printf("Error updating picks: %v", err)
     return
   }
 
@@ -110,9 +90,4 @@ func PicksUpdateHandler(w http.ResponseWriter, r *http.Request) {
   w.WriteHeader(http.StatusOK)
 }
 
-type PicksUpdateRequest []PicksUpdatePick
-
-type PicksUpdatePick struct {
-  Category string `json:"category"`
-  Pick string `json:"pick"`
-}
+type PicksUpdateRequest map[string]string
